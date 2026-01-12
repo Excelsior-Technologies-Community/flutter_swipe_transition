fab69c92f7029b893e1c43aa59d6c0c980b1db64
mathukiya_hit <mathukiyaheet5@gmail.com>
2026-01-12 15:44:23 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/flutter_swipe_transition/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_swipe_transition;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Swipe Transition',
+    );
+  }
+}
